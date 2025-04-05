1217afe019273796701caa1ebc5eebac9fecb999
Chethiya Ravindranath <121000739+ch3thiya@users.noreply.github.com>
2025-04-05 10:55:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
         loadingIndicator.classList.add('active');
         
         try {
-            const apiKey = process.env.GOOGLE_TRANSLATE_API_KEY;
+            const apiKey = AIzaSyDw0KMvl0ku3rfkAjHjZjWq-ruxa0gQdgc;
             const url = 'https://translation.googleapis.com/language/translate/v2';
             
             const params = new URLSearchParams({
